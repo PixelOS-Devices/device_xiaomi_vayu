9c17eab0af6e66a9ce193cce277ecd78178c61c8
bgcngm <bgcngm@gmail.com>
2021-10-29 12:18:04 +0530

    vayu: livedisplay: Implement SunlightEnhancement
    Signed-off-by: clhex <clhexftw@protonmail.com>

--- device.mk
+++ device.mk
@@ -333,6 +333,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.vayu
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm \
+    vendor.lineage.livedisplay@2.1-service.vayu
+
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
