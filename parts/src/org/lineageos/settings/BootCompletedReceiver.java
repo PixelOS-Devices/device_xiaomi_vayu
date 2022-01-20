992054961e4c2e1d44838c4140b424daad626067
Lostark13 <tushar9915@gmail.com>
2022-01-20 14:54:47 +0530

    vayu: parts: Add per app maximum refresh rate
    -based on commits 9cd2ebf[1] , 87a43c0[2] , d795e4f[3]
    
    ref:
    [1] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/9cd2ebf76d86a64eccbc934ca72751398977e8bc
    [2] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/87a43c0f4170d23db01cd3f5cdb657130edd136b
    [3] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/d795e4f05cb31712e6c4b15760473316b1b4bd80
    
    Original commit:
    https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/c8c200e44a0dae702c459597b48c5b1c5a3f5972
    
    [someone5678]
    * move the location from display settings to devicesettings
      now it's located under Refresh Rate category
    * add main icon
    * change theme to SubSettingsBase
    
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    vayu: parts: refreshrate: Improvement Refresh Rate per-app
    
    chrisw444:
    [1] - Adapt to A12 Style
    [2] - Improvemnt strings title and sumary
    [3] - Add pt-rBR translations
    
    rdxzv:
    [1] - Use thermal profiles icon for default rate icon
    [2] - Fix some icons disappearing on dark mode
    
    someone5678:
    [1] - change title to Refresh Rate Profiles
    ref: https://github.com/PixelExperience-Devices/device_xiaomi_surya/commit/dfb60991cd5c4899666f17097b2c424ef3f9e36e
    
    Co-authored-by: rdxzv <rdxzv.dev@gmail.com>
    
    vayu: parts: refreshrate: Replace standard to medium refresh rate
    
    vayu: parts: refreshrate: Improve refresh rate per app
    
    vayu: parts: refreshrate: set correct default refresh rate
    
    [1]
    * import Constants
    * set REFRESH_STATE_DEFAULT as DEFAULT_REFRESH_RATE
    
    [2] vayu: parts: Kang Constants.java
    src: https://github.com/someone5678/device_xiaomi_sweet/blob/4904f894ea2426acb2a8fd7fb11744342d2f488b/devicesettings/src/org/lineageos/settings/device/Constants.java#L8-L9
    
    vayu: parts: refreshrate: complete improvement/adaption to S style
    
    * complete app list improvement [1]
    * complete adaption to S style [2]
    * add ACTION_SCREEN_ON intent filter [3]
    
    * keep layout_width of spinner value to wrap_content
      as it's too wide with match_parent
    
    ref:
    [1] https://github.com/someone5678/device_xiaomi_sweet/commit/845f890001f649ea2168a96b6cb65b6a515a81da
    [2] https://github.com/someone5678/device_xiaomi_sweet/commit/547b7d9dc270f9c6c323b26be2ae6b4995f42610
    [3] https://github.com/someone5678/device_xiaomi_sweet/commit/c652c868b31d73a7ea6c9ddebba5469cd7e06c2b
    
    Signed-off-by: someone5678 <nemui3353@gmail.com>
    
    vayu: parts: refreshrate: Improve refresh-rate strings

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -27,6 +27,7 @@ import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -44,5 +45,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         HapticUtils.restoreLevel(context);
+        RefreshUtils.startService(context);
     }
 }
