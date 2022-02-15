6735494d28b1f29f3acab524227ae31c35e66ad8
Nauval Rizky <enuma.alrizky@gmail.com>
2022-02-15 21:33:05 +0700

    vayu: parts: Describe current Dirac state to the tile

--- parts/src/org/lineageos/settings/dirac/DiracTileService.java
+++ parts/src/org/lineageos/settings/dirac/DiracTileService.java
@@ -1,38 +1,41 @@
 package org.lineageos.settings.dirac;
 
+import android.content.Context;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
+
+import org.lineageos.settings.R;
 
 public class DiracTileService extends TileService {
 
     private DiracUtils mDiracUtils;
+    private Context mContext;
 
     @Override
     public void onStartListening() {
-        mDiracUtils = DiracUtils.getInstance(getApplicationContext());
+        mContext = getApplicationContext();
+        mDiracUtils = DiracUtils.getInstance(mContext);
 
-        Tile tile = getQsTile();
-        if (mDiracUtils.isDiracEnabled()) {
-            tile.setState(Tile.STATE_ACTIVE);
-        } else {
-            tile.setState(Tile.STATE_INACTIVE);
-        }
-
-        tile.updateTile();
+        updateTileContent(mDiracUtils.isDiracEnabled());
         super.onStartListening();
     }
 
     @Override
     public void onClick() {
-        Tile tile = getQsTile();
-        if (mDiracUtils.isDiracEnabled()) {
-            mDiracUtils.setEnabled(false);
-            tile.setState(Tile.STATE_INACTIVE);
-        } else {
-            mDiracUtils.setEnabled(true);
-            tile.setState(Tile.STATE_ACTIVE);
-        }
-        tile.updateTile();
+        boolean isEnabled = mDiracUtils.isDiracEnabled();
+        mDiracUtils.setEnabled(!isEnabled);
+        updateTileContent(!isEnabled);
         super.onClick();
+    }
+
+    private void updateTileContent(boolean isActive) {
+        Tile tile = getQsTile();
+        String on = mContext.getResources().getString(R.string.on);
+        String off = mContext.getResources().getString(R.string.off);
+
+        tile.setState(isActive ? Tile.STATE_ACTIVE : Tile.STATE_INACTIVE);
+        tile.setContentDescription(isActive ? on : off);
+        tile.setSubtitle(isActive ? on : off);
+        tile.updateTile();
     }
 }
