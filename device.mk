9d181d03990a26f5a58481d60201690d55000924
Bruno Martins <bgcngm@gmail.com>
2022-08-09 16:07:59 +0530

    vayu: Switch to Common Xiaomi Fingerprint HIDL
    Change-Id: Id1a44a76f5e2c132ab8725fc2481a6776b912539
    Signed-off-by: clhex <clhexftw@protonmail.com>

--- device.mk
+++ device.mk
@@ -192,7 +192,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.vayu \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     libkeymaster_messages.vendor
 
