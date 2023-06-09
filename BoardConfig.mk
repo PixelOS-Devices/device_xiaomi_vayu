329bb1a083d0124b99f704d96bab6e4e40184628
clhex <clhexftw@protonmail.com>
2023-06-09 22:06:18 +0530

    vayu: Update from miui_VAYUGlobal_V14.0.3.0.TJUMIXM_6be397120d_13.0.zip
    - Update Blobs
    - Update Build fp and desc
    - Update Vendor SPL
    
    Change-Id: Iec38a95a4cddf98c2db507373acf64f6e8d027a0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,7 +172,7 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2022-07-01
+VENDOR_SECURITY_PATCH := 2023-04-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
