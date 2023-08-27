f1cc10a82b41581fc770aea90f4bb4ba8cd78d73
clhex <clhexftw@protonmail.com>
2023-08-27 11:55:32 +0530

    vayu: Import stock apn config

--- device.mk
+++ device.mk
@@ -302,6 +302,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
