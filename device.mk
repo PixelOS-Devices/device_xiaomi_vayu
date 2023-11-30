b3fa07d61676ee095fe602b309b559ba8b9f97df
Bruno Martins <bgcngm@gmail.com>
2023-11-30 21:20:43 +0530

    vayu: Uprev android.hardware.drm to 1.4
    Change-Id: Ia583adb7f3e8a75d103a344407315a13a2982d60

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
